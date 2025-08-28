9f1114d4e1c04c9844a6582dc176abc583efb67c
KW Kwan <kw.kwan@ribose.com>
2025-08-28 17:01:18 +0800

    Remove unneeded relative_file_path

--- lib/metanorma/plugin/plantuml/utils.rb
+++ lib/metanorma/plugin/plantuml/utils.rb
@@ -13,13 +13,6 @@ module Metanorma
               document.attributes["docdir"] ||
               File.dirname(document.attributes["docfile"] || ".")
           end
-
-          def relative_file_path(document, file_path)
-            return file_path if Pathname.new(file_path).absolute?
-
-            docdir = document.attributes["docdir"] || Dir.pwd
-            File.expand_path(file_path, docdir)
-          end
         end
       end
     end
