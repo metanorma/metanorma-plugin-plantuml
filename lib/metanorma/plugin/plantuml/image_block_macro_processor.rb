7cf9d75dd027771f8cc37784b9f6a0949c9c9c12
KW Kwan <kw.kwan@ribose.com>
2025-09-03 16:33:53 +0800

    New command: plantuml_image::{path}[{options}]

---
+++ lib/metanorma/plugin/plantuml/image_block_macro_processor.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+module Metanorma
+  module Plugin
+    module Plantuml
+      # PlantUML block processor for Asciidoctor
+      class ImageBlockMacroProcessor < ::Asciidoctor::Extensions::BlockMacroProcessor
+        include ::Metanorma::Plugin::Plantuml::BlockProcessorBase
+        use_dsl
+        named :plantuml_image
+
+        def process(parent, reader, attrs) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+          # Check for document-level disable flag
+          if parent.document.attr("plantuml-disabled")
+            return abort(parent, reader, attrs, "PlantUML processing disabled")
+          end
+
+          # Check PlantUML availability explicitly
+          unless Backend.plantuml_available?
+            return abort(parent, reader, attrs, "PlantUML not installed")
+          end
+
+          # Parse format specifications
+          formats = parse_formats(attrs, parent.document)
+          options = parse_options(parent, reader, attrs)
+
+          process_image_block(parent, reader, attrs, formats, options)
+        rescue StandardError => e
+          abort(parent, reader, attrs, e.message)
+        end
+
+        private
+
+        def add_image_path_to_includedirs(document, image_path, includedirs)
+          docdir = document.attributes["docdir"]
+          includedirs << File.dirname(File.join(docdir, image_path))
+          includedirs.compact.uniq
+        end
+
+        def parse_options(parent, reader, attrs)
+          options = {}
+
+          # Parse include directory
+          options[:includedirs] = parse_doc_includedirs(parent.document)
+          options[:includedirs] = add_attrs_to_includedirs(
+            parent.document, attrs, options[:includedirs]
+          )
+          options[:includedirs] = add_image_path_to_includedirs(
+            parent.document, reader, options[:includedirs]
+          )
+
+          options
+        end
+      end
+    end
+  end
+end
