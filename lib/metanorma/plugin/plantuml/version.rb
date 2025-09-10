7ef0110fe3629faaeffd81500fd1823c39203a53
KW Kwan <kw.kwan@ribose.com>
2025-09-10 12:53:06 +0800

    Update plantuml jar

--- lib/metanorma/plugin/plantuml/version.rb
+++ lib/metanorma/plugin/plantuml/version.rb
@@ -2,7 +2,7 @@ module Metanorma
   module Plugin
     module Plantuml
       VERSION = "1.0.4".freeze
-      PLANTUML_JAR_VERSION = "1.2025.4".freeze
+      PLANTUML_JAR_VERSION = "1.2025.7".freeze
     end
   end
 end
