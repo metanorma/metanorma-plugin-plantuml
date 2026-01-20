3e5bb8f5fcbee84838da981feb2ca72f0a614225
KW Kwan <kw.kwan@ribose.com>
2026-01-20 16:09:11 +0800

    Modify the priority to locate localdir

--- lib/metanorma/plugin/plantuml/utils.rb
+++ lib/metanorma/plugin/plantuml/utils.rb
@@ -9,9 +9,13 @@ module Metanorma
       module Utils
         class << self
           def localdir(document)
-            document.attributes["localdir"] ||
+            if document.attributes["docfile"]
+              File.dirname(document.attributes["docfile"])
+            else
               document.attributes["docdir"] ||
-              File.dirname(document.attributes["docfile"] || ".")
+                document.attributes["localdir"] ||
+                File.dirname(".")
+            end
           end
         end
       end
