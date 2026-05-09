96dd5741b0dde411130e1fb2aca1c23765be0fda
kwkwan <kwkwan@users.noreply.github.com>
2026-05-09 06:44:57 +0000

    Update PlantUML to version 1.2026.3
    - Updated PLANTUML_JAR_VERSION to 1.2026.3
    - Downloaded and verified new PlantUML JAR file
    - Incremented gem version to 1.0.11
    
    Generated by automated PlantUML update workflow

--- lib/metanorma/plugin/plantuml/version.rb
+++ lib/metanorma/plugin/plantuml/version.rb
@@ -4,7 +4,7 @@ module Metanorma
   module Plugin
     module Plantuml
       VERSION = "1.0.12"
-      PLANTUML_JAR_VERSION = "1.2026.2"
+      PLANTUML_JAR_VERSION = "1.2026.3"
     end
   end
 end
